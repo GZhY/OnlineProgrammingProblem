72ebadd2abc0a5a169109b1199f9a26c6dbd076e
GZhY <gaoz_y@163.com>
2016-10-11 21:15:13 +0800

    Problemset #1000 init

---
+++ Problemset/1000/main.cpp
@@ -0,0 +1,15 @@
+/*
+* @Author: GZhY
+* @Time:   2016-10-11 21:00:00
+* @Last Modified by:   GZhY
+* @Last Modified time: 2016-10-11 21:00:14
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
