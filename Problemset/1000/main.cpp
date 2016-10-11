6d29bd9e29ea511c136623cf38e941c98ad8bdfb
GZhY <gaoz_y@163.com>
2016-10-11 21:26:03 +0800

    P#1000: AC

--- Problemset/1000/main.cpp
+++ Problemset/1000/main.cpp
@@ -2,14 +2,14 @@
 * @Author: GZhY
 * @Time:   2016-10-11 21:00:00
 * @Last Modified by:   GZhY
-* @Last Modified time: 2016-10-11 21:00:14
+* @Last Modified time: 2016-10-11 21:19:18
 */
 
 #include <iostream>
-
-using namespace std;
-
 int main(){
-
+	int a,b;
+	while(std::cin>>a>>b){
+		std::cout << a+b << '\n';
+	}
     return 0;
 }
