1d6eebaca9528a1b37002465e9b06256bf8b01cc
GZhY <gaoz_y@163.com>
2016-10-12 23:44:18 +0800

    P#1014 init

---
+++ Problemset/1014/main.cpp
@@ -0,0 +1,15 @@
+/*
+* @Author: GZhY
+* @Time:   2016-10-12 23:43:01
+* @Last Modified by:   GZhY
+* @Last Modified time: 2016-10-12 23:43:14
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
