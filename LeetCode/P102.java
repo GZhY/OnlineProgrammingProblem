960a6aa5f724ff6c1ca050c4e9462ded3d8d2f23
GZhY <gaoz_y@163.com>
2016-10-18 23:57:09 +0800

    P102 AC

--- LeetCode/P102.java
+++ LeetCode/P102.java
@@ -2,7 +2,7 @@
 * @Author: zy
 * @Date:   2016-10-18 22:27:45
 * @Last Modified by:   zy
-* @Last Modified time: 2016-10-18 23:45:25
+* @Last Modified time: 2016-10-18 23:55:35
 */
 
 /**
@@ -19,17 +19,24 @@ public class Solution {
         List<List<Integer>> intList = new ArrayList<>();
         if(root == null) return intList;
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        Queue<int> level = new LinkedList<TreeNode>();
+        Queue<Integer> level = new LinkedList<Integer>();
         queue.offer(root);
+        level.offer(0);
         intList.add(new ArrayList<Integer>());
         while(!queue.isEmpty()){
         	TreeNode treeNode = queue.poll();
+        	int currentLevel = level.poll();
         	if(currentLevel+1 > intList.size())
         		intList.add(new ArrayList<Integer>());
         	intList.get(currentLevel).add(treeNode.val);
-        	if(treeNode.left != null) queue.offer(treeNode.left);
-        	if(treeNode.right != null) queue.offer(treeNode.right);
-        	if(treeNode.left!=null || treeNode.right != null) currentLevel++;
+        	if(treeNode.left != null) {
+        		queue.offer(treeNode.left);
+        		level.offer(currentLevel+1);
+        	}
+        	if(treeNode.right != null) {
+        		queue.offer(treeNode.right);
+        		level.offer(currentLevel+1);
+        	}
         }
         return intList;
     }
