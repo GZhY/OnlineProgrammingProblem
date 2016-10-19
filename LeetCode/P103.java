4bf29a3a936d6f11fe867adc7e13d42f6529f9d1
GZhY <gaoz_y@163.com>
2016-10-19 14:58:56 +0800

    P103 AC Runtime: 2ms

--- LeetCode/P103.java
+++ LeetCode/P103.java
@@ -2,10 +2,11 @@
 * @Author: zy
 * @Date:   2016-10-19 14:53:54
 * @Last Modified by:   zy
-* @Last Modified time: 2016-10-19 14:56:06
+* @Last Modified time: 2016-10-19 14:57:54
 */
 
 /**
+ * Runtime: 2ms
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -23,14 +24,11 @@ public class Solution {
     private void zigzagLevelRecursion(TreeNode node, List<List<Integer>> result, int level) {
         if (node == null) return;
         if (result.size() < level + 1) 
-            result.add(new ArrayList<Integer>());
-        result.get(level).add(node.val);
-        if(level%2 != 0){
-            zigzagLevelRecursion(node.left, result, level + 1);
-            zigzagLevelRecursion(node.right, result, level + 1);
-        }else{
-            zigzagLevelRecursion(node.right, result, level + 1);
-            zigzagLevelRecursion(node.left, result, level + 1);
-        }
+            result.add(new LinkedList<Integer>());
+        if(level%2 != 0)
+            ((LinkedList<Integer>)result.get(level)).addFirst(node.val);
+        else result.get(level).add(node.val);
+        zigzagLevelRecursion(node.left, result, level + 1);
+        zigzagLevelRecursion(node.right, result, level + 1);
     }
 }
