cfff66d06b55b0c838b6228ae42bbc475573f686
GZhY <gaoz_y@163.com>
2016-10-22 00:16:23 +0800

    P77 AC Runtime: 9 ms

--- LeetCode/P77.java
+++ LeetCode/P77.java
@@ -2,9 +2,10 @@
 * @Author: GZhY
 * @Date:   2016-10-21 23:46:52
 * @Last Modified by:   GZhY
-* @Last Modified time: 2016-10-22 00:08:15
+* @Last Modified time: 2016-10-22 00:16:06
 */
 
+// Runtime: 9 ms
 public class Solution {
 	public List<Integer> nextCombine(List<Integer> iList, int n, int k) {
 		for (int i = k-1; i >= 0; --i) {
@@ -25,8 +26,8 @@ public class Solution {
 			iList.add(i);
 		result.add(new ArrayList<Integer>(iList));
 		while (true) {
+		    if(iList.get(0) == n-k+1) break;
 			result.add(nextCombine(iList, n, k));
-			if(iList.get(0) == n-k+1) break;
 		}
 		return result;
 
