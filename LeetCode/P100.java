03dd4b4f900c9b743ae7b07ef616d4992fdf9cf4
GZhY <gaoz_y@163.com>
2016-10-26 21:38:50 +0800

    P100 Runtime:0ms Beats:19.68%

--- LeetCode/P100.java
+++ LeetCode/P100.java
@@ -2,16 +2,14 @@
 * @Author: GZhY
 * @Time:   2016-10-26 21:20:43
 * @Last Modified by:   GZhY
-* @Last Modified time: 2016-10-26 21:21:30
+* @Last Modified time: 2016-10-26 21:35:23
 */
 
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null)
-            if(q == null) return true;
-            else return false;
-        if(q == null) return false;
-        if(p.val != q.val) return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if(p == null && q == null) return true;
+        if(p != null && q != null && p.val == q.val) 
+        	return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    	return false;
     }
 }
