4fc3f17e779fa0496ddc3a6808bac9d30363b71f
GZhY <gaoz_y@163.com>
2016-10-28 12:27:37 +0800

    P388 AC Runtime:7ms Beats:23.28% 'Use HashMap to restore file system'

---
+++ LeetCode/P388.java
@@ -0,0 +1,21 @@
+/*
+* @Author: GZhY
+* @Time:   2016-10-28 12:22:44
+* @Last Modified by:   GZhY
+* @Last Modified time: 2016-10-28 12:23:39
+*/
+
+public class Solution {
+    public int lengthLongestPath(String input) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        hashMap.put(0, 0);
+        int result = 0;
+        for (String s : input.split("\n")) {
+            int level = s.lastIndexOf("\t") + 1;
+            int len = s.substring(level).length();
+            if (s.contains(".")) result = Math.max(result, hashMap.get(level) + len);
+            else hashMap.put(level + 1, hashMap.get(level) + len + 1);
+        }
+        return result;
+    }
+}
