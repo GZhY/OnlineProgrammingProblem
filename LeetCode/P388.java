53575a075e9090990269e65ca00b489052e3cdb9
GZhY <gaoz_y@163.com>
2016-10-28 12:28:34 +0800

    add Runtime & Beats

--- LeetCode/P388.java
+++ LeetCode/P388.java
@@ -2,10 +2,11 @@
 * @Author: GZhY
 * @Time:   2016-10-28 12:22:44
 * @Last Modified by:   GZhY
-* @Last Modified time: 2016-10-28 12:23:39
+* @Last Modified time: 2016-10-28 12:28:02
 */
 
 public class Solution {
+	// Runtime:7ms Beats:23.28%
     public int lengthLongestPath(String input) {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         hashMap.put(0, 0);
