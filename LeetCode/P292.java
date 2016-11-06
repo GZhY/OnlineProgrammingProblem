6f71d0982637ff3258059181188513eb297ebb9a
GZhY <gaoz_y@163.com>
2016-11-07 00:09:10 +0800

    P292 AC 'use bit manipulation'

--- LeetCode/P292.java
+++ LeetCode/P292.java
@@ -2,12 +2,13 @@
 * @Author: GZhY
 * @Date:   2016-11-06 23:58:19
 * @Last Modified by:   GZhY
-* @Last Modified time: 2016-11-06 23:59:43
+* @Last Modified time: 2016-11-07 00:07:24
 */
 
 // Runtime: 0 ms
 public class Solution {
     public boolean canWinNim(int n) {
-        return !(n%4==0);
+        return (n&3)!=0;
+        // return !(n%4==0);
     }
 }
