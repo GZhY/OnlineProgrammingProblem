6d2868e294c74284284ec342a02d2287d2c4c22b
GZhY <gaoz_y@163.com>
2016-11-06 23:48:01 +0800

    P371 AC Runtime:0ms

--- LeetCode/P371.java
+++ LeetCode/P371.java
@@ -2,9 +2,12 @@
 * @Author: GZhY
 * @Date:   2016-11-06 23:43:36
 * @Last Modified by:   GZhY
-* @Last Modified time: 2016-11-06 23:43:52
+* @Last Modified time: 2016-11-06 23:47:40
 */
 
+// Runtime: 0 ms
+// A summary: how to use bit manipulation to solve problems easily and efficiently
+// https://discuss.leetcode.com/topic/50315/a-summary-how-to-use-bit-manipulation-to-solve-problems-easily-and-efficiently
 public class Solution {
     public int getSum(int a, int b) {
         return b==0? a:getSum(a^b, (a&b)<<1);
