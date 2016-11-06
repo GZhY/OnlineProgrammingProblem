46de5d38a9e497e90910bad0a05147c77e04c3b3
GZhY <gaoz_y@163.com>
2016-11-06 23:59:48 +0800

    P292 AC Runtime:0ms

--- LeetCode/P292.java
+++ LeetCode/P292.java
@@ -2,9 +2,10 @@
 * @Author: GZhY
 * @Date:   2016-11-06 23:58:19
 * @Last Modified by:   GZhY
-* @Last Modified time: 2016-11-06 23:58:27
+* @Last Modified time: 2016-11-06 23:59:43
 */
 
+// Runtime: 0 ms
 public class Solution {
     public boolean canWinNim(int n) {
         return !(n%4==0);
