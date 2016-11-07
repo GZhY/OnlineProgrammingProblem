a9f8a4893297ae0f92a7df2b4c3869d7ee88e699
GZhY <gaoz_y@163.com>
2016-11-08 00:19:55 +0800

    P389 AC Runtime:6ms Beats:86.84%

--- LeetCode/P389.java
+++ LeetCode/P389.java
@@ -2,16 +2,27 @@
 * @Author: GZhY
 * @Date:   2016-11-08 00:11:10
 * @Last Modified by:   GZhY
-* @Last Modified time: 2016-11-08 00:11:47
+* @Last Modified time: 2016-11-08 00:19:04
 */
 
 public class Solution {
-    public char findTheDifference(String s, String t) {
-    	// Runtime: 9 ms
-        char ans = t.charAt(t.length() - 1);
-        for(int i = 0; i < s.length(); i++){
-            ans ^= s.charAt(i) ^ t.charAt(i);
-        }
+
+	// Runtime: 6 ms Beats:86.84%
+	public char findTheDifference(String s, String t) {
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        char ans = tArr[sArr.length];
+        for(int i = 0; i < sArr.length; ++i)
+            ans ^= sArr[i]^tArr[i];
         return ans;
     }
+
+    // Runtime: 9 ms Beats:40.44%
+    // public char findTheDifference(String s, String t) {
+    //     char ans = t.charAt(t.length() - 1);
+    //     for(int i = 0; i < s.length(); i++){
+    //         ans ^= s.charAt(i) ^ t.charAt(i);
+    //     }
+    //     return ans;
+    // }
 }
