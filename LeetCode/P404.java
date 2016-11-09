ccfddab1a9d732c76565096af3d167b0c8ccd41c
GZhY <gaoz_y@163.com>
2016-11-09 23:50:11 +0800

    P404 AC Runtime:10ms Beats:24.54%

---
+++ LeetCode/P404.java
@@ -0,0 +1,13 @@
+/*
+* @Author: GZhY
+* @Date:   2016-11-09 23:46:58
+* @Last Modified by:   GZhY
+* @Last Modified time: 2016-11-09 23:49:03
+*/
+
+// Runtime:10ms Beats:24.54%
+public class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return root == null ? 0 : ((root.left != null && root.left.left == null && root.left.right == null) ? root.left.val+sumOfLeftLeaves(root.right) : sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right));
+    }
+}
