e08d6f1a8a9e50f095af2b71ad45421bbefc02a3
GZhY <gaoz_y@163.com>
2016-11-14 23:56:20 +0800

    P283 AC

---
+++ LeetCode/P283.java
@@ -0,0 +1,17 @@
+/*
+* @Author: GZhY
+* @Date:   2016-11-14 23:52:09
+* @Last Modified by:   GZhY
+* @Last Modified time: 2016-11-14 23:55:12
+*/
+
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums == null || nums.length < 2) return;
+        int index = 0;
+        for (int num: nums)
+        	if(num != 0) nums[index++] = num;
+        while(index < nums.length)
+        	nums[index++] = 0;
+    }
+}
