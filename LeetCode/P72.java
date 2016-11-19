489370c0c02983796fc70b1b57562f3b22521f01
GZhY <gaoz_y@163.com>
2016-11-19 23:14:16 +0800

    P72 add trick

--- LeetCode/P72.java
+++ LeetCode/P72.java
@@ -2,7 +2,7 @@
 * @Author: GZhY
 * @Date:   2016-11-19 22:18:36
 * @Last Modified by:   GZhY
-* @Last Modified time: 2016-11-19 22:20:12
+* @Last Modified time: 2016-11-19 23:12:07
 */
 // Runtime:7ms Beats 98.86%
 public class Solution {
@@ -12,6 +12,17 @@ public class Solution {
 
     public int minDistance(String word1, String word2) {
         int i = word1.length(), j = word2.length();
+        // These codes that line between 9 and 17 can be used to skip the common prefixes in linear time.
+        int same = 0;
+        for (int k = 0; k < i && k < j; k++) {
+            if(word1.charAt(k) == word2.charAt(k)) same++;
+            else break;
+        }
+        i -= same;
+        j -= same;
+        word1 = word1.substring(same);
+        word2 = word2.substring(same);
+
         dp = new int[i][j];
         return minDistanceRecursion(word1, word2, i - 1, j - 1);
     }
