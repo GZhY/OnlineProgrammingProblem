ff48fd3660def5b3342b774ae472a2dc6abc1e69
GZhY <gaoz_y@163.com>
2017-01-14 16:35:16 +0800

    P461 AC Runtime:10ms Beats:81.42%

--- LeetCode/P461.java
+++ LeetCode/P461.java
@@ -2,9 +2,10 @@
 * @Author: GZhY
 * @Date:   2017-01-14 16:20:40
 * @Last Modified by:   GZhY
-* @Last Modified time: 2017-01-14 16:21:05
+* @Last Modified time: 2017-01-14 16:25:11
 */
 
+// Runtime:10ms Beats:81.42%
 public class Solution {
     public int hammingDistance(int x, int y) {
         return Integer.bitCount(x ^ y);
