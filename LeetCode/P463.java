e33fc169e73d4f3d15b89bef6668fc65353720d0
GZhY <gaoz_y@163.com>
2017-01-15 23:59:50 +0800

    P463 AC

--- LeetCode/P463.java
+++ LeetCode/P463.java
@@ -2,13 +2,12 @@
 * @Author: GZhY
 * @Time:   2017-01-14 19:51:00
 * @Last Modified by:   GZhY
-* @Last Modified time: 2017-01-15 23:57:43
+* @Last Modified time: 2017-01-15 23:59:30
 */
 
 public class Solution {
     public int islandPerimeter(int[][] grid) {
         int islands = 0, neighbours = 0;
-
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 1) {
@@ -18,7 +17,6 @@ public class Solution {
                 }
             }
         }
-
         return islands * 4 - neighbours * 2;
     }
 	//Runtime:244ms Beats:1.8%
