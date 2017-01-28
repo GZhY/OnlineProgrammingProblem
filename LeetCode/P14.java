27c7ea7b570e89812c672f8a296738a24064bbfa
GZhY <gaoz_y@163.com>
2017-01-28 21:48:02 +0800

    P14 AC

---
+++ LeetCode/P14.java
@@ -0,0 +1,28 @@
+/*
+* @Author: GZhY
+* @Date:   2017-01-28 21:47:09
+* @Last Modified by:   GZhY
+* @Last Modified time: 2017-01-28 21:47:18
+*/
+
+public class Solution {
+    public String longestCommonPrefix(String str1, String str2) {
+        int index = 0;
+        for (int i = 0; i < str1.length() && i < str2.length(); i++) {
+            if(str1.charAt(i) == str2.charAt(i)) index++;
+            else break;
+        }
+        return str1.substring(0, index);
+    }
+    
+    public String longestCommonPrefix(String[] strs) {
+        String s = null;
+        for (String str : strs) {
+            if(s == null) s = str;
+            else{
+                s = longestCommonPrefix(s, str);
+            }
+        }
+        return s == null ? "" : s;
+    }
+}
