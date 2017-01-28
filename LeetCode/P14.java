de7edc3843a6512a9ca5ee352afd7a86f47786b5
GZhY <gaoz_y@163.com>
2017-01-28 22:06:12 +0800

    P14 AC Line:6 Runtime:9ms, Beats:93.59%

--- LeetCode/P14.java
+++ LeetCode/P14.java
@@ -2,28 +2,40 @@
 * @Author: GZhY
 * @Date:   2017-01-28 21:47:09
 * @Last Modified by:   GZhY
-* @Last Modified time: 2017-01-28 21:49:04
+* @Last Modified time: 2017-01-28 22:03:40
 */
 
-// Runtime:15ms, Beats:17.50%
+
 public class Solution {
-    public String longestCommonPrefix(String str1, String str2) {
-        int index = 0;
-        for (int i = 0; i < str1.length() && i < str2.length(); i++) {
-            if(str1.charAt(i) == str2.charAt(i)) index++;
-            else break;
-        }
-        return str1.substring(0, index);
+
+	// Runtime:9ms, Beats:93.59%
+	public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String pre = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(pre) != 0)
+                pre = pre.substring(0, pre.length() - 1);
+        return pre;
     }
+
+	// // Runtime:15ms, Beats:17.50%
+ //    public String longestCommonPrefix(String str1, String str2) {
+ //        int index = 0;
+ //        for (int i = 0; i < str1.length() && i < str2.length(); i++) {
+ //            if(str1.charAt(i) == str2.charAt(i)) index++;
+ //            else break;
+ //        }
+ //        return str1.substring(0, index);
+ //    }
     
-    public String longestCommonPrefix(String[] strs) {
-        String s = null;
-        for (String str : strs) {
-            if(s == null) s = str;
-            else{
-                s = longestCommonPrefix(s, str);
-            }
-        }
-        return s == null ? "" : s;
-    }
+ //    public String longestCommonPrefix(String[] strs) {
+ //        String s = null;
+ //        for (String str : strs) {
+ //            if(s == null) s = str;
+ //            else{
+ //                s = longestCommonPrefix(s, str);
+ //            }
+ //        }
+ //        return s == null ? "" : s;
+ //    }
 }
