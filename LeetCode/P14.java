492d65d4961753a364fe1b0076060576d372d159
GZhY <gaoz_y@163.com>
2017-01-28 22:17:17 +0800

    P14 AC Use:Sort Runtime:13ms

--- LeetCode/P14.java
+++ LeetCode/P14.java
@@ -2,21 +2,31 @@
 * @Author: GZhY
 * @Date:   2017-01-28 21:47:09
 * @Last Modified by:   GZhY
-* @Last Modified time: 2017-01-28 22:03:40
+* @Last Modified time: 2017-01-28 22:16:50
 */
 
 
 public class Solution {
 
-	// Runtime:9ms, Beats:93.59%
+	// Runtime:13ms, Beats:35.58%
 	public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
-        String pre = strs[0];
-        for (int i = 1; i < strs.length; i++)
-            while (strs[i].indexOf(pre) != 0)
-                pre = pre.substring(0, pre.length() - 1);
-        return pre;
+    	if (strs == null || strs.length == 0) return "";
+    	Arrays.sort(strs);
+    	String a = strs[0], b = strs[strs.length - 1];
+        while (b.indexOf(a) != 0)
+            a = a.substring(0, a.length() - 1);
+        return a;
     }
+
+	// // Runtime:9ms, Beats:93.59%
+	// public String longestCommonPrefix(String[] strs) {
+ //        if (strs == null || strs.length == 0) return "";
+ //        String pre = strs[0];
+ //        for (int i = 1; i < strs.length; i++)
+ //            while (strs[i].indexOf(pre) != 0)
+ //                pre = pre.substring(0, pre.length() - 1);
+ //        return pre;
+ //    }
 
 	// // Runtime:15ms, Beats:17.50%
  //    public String longestCommonPrefix(String str1, String str2) {
