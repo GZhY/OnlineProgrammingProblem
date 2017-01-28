80df2ed58d3033db5da83045bbbee8851c96488f
GZhY <gaoz_y@163.com>
2017-01-28 22:22:10 +0800

    P14 explanation

--- LeetCode/P14.java
+++ LeetCode/P14.java
@@ -2,31 +2,34 @@
 * @Author: GZhY
 * @Date:   2017-01-28 21:47:09
 * @Last Modified by:   GZhY
-* @Last Modified time: 2017-01-28 22:16:50
+* @Last Modified time: 2017-01-28 22:20:21
 */
 
 
 public class Solution {
-
-	// Runtime:13ms, Beats:35.58%
-	public String longestCommonPrefix(String[] strs) {
-    	if (strs == null || strs.length == 0) return "";
-    	Arrays.sort(strs);
-    	String a = strs[0], b = strs[strs.length - 1];
-        while (b.indexOf(a) != 0)
-            a = a.substring(0, a.length() - 1);
-        return a;
-    }
-
-	// // Runtime:9ms, Beats:93.59%
-	// public String longestCommonPrefix(String[] strs) {
- //        if (strs == null || strs.length == 0) return "";
- //        String pre = strs[0];
- //        for (int i = 1; i < strs.length; i++)
- //            while (strs[i].indexOf(pre) != 0)
- //                pre = pre.substring(0, pre.length() - 1);
- //        return pre;
+	/**
+	* Runtime:13ms, Beats:35.58%
+	* Comparison between two strings are O(k), k is the length of two strings,
+	* so the complexity of this algorithm is O(nlogn * k), not O(nlogn).
+	**/
+ //	public String longestCommonPrefix(String[] strs) {
+ // 	if (strs == null || strs.length == 0) return "";
+ // 	Arrays.sort(strs);
+ // 	String a = strs[0], b = strs[strs.length - 1];
+ //        while (b.indexOf(a) != 0)
+ //            a = a.substring(0, a.length() - 1);
+ //        return a;
  //    }
+
+	// Runtime:9ms, Beats:93.59%
+	public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String pre = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(pre) != 0)
+                pre = pre.substring(0, pre.length() - 1);
+        return pre;
+    }
 
 	// // Runtime:15ms, Beats:17.50%
  //    public String longestCommonPrefix(String str1, String str2) {
