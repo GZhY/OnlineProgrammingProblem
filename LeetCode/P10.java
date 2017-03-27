4c35657a9f23c9c931ef5d69058cb4b4fbaf5cd8
GZhY <gaoz_y@163.com>
2017-03-27 20:02:54 +0800

    P10 WA

---
+++ LeetCode/P10.java
@@ -0,0 +1,28 @@
+/*
+* @Author: GZhY
+* @Time:   2017-03-27 19:57:45
+* @Last Modified by:   GZhY
+* @Last Modified time: 2017-03-27 20:02:37
+*/
+
+public class Solution {
+
+	// WA "ab" \n ".*c"
+    public boolean isMatch(String s, String p) {
+        if("".equals(p)) return "".equals(s) ? true : false;
+        if(s.length() > 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.')){
+            if(p.length()>=2 && p.charAt(1) == '*'){
+               String temp = p.substring(0,2);
+               p = p.replaceFirst("("+p.charAt(0)+"\\*)*", "");
+               String p2 = p.replaceFirst(temp, "");
+               String s2 = s.replaceFirst(temp, "");
+               if(p.length()-p2.length() > s.length()-s2.length()) return false;
+               return isMatch(s2, p2); 
+            }
+            return isMatch(s.substring(1), p.substring(1));
+        }
+        if(p.length()>=2 && p.charAt(1) == '*')
+                return isMatch(s, p.substring(2));
+        return false;
+    }
+}
